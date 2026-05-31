b92ba0cac7527043f7deff3fd301d9adf24b4331
Deporedo <gshishshs@mail.ru>
2026-05-31 09:32:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ let currentPhase = 'wait'; // Возможные фазы: 'wait', 'flight', 'cr
 let phaseStartTime = Date.now();
 let targetMultiplier = 1.00;
 
+// Справочник подключенных пользователей (username -> socket.id)
+// Необходим для того, чтобы сервер знал, кому именно начислять TON и NFT
+const connectedUsers = {};
+
 // Математическая функция генерации точки взрыва (Crash)
 function generateCrashMultiplier() {
     const r = Math.random();
@@ -52,8 +56,7 @@ function startFlightPhase() {
         targetMultiplier: targetMultiplier
     });
 
-    // Вычисляем точное время полёта до точки краша по формуле из клиента:
-    // mult = Math.pow(1.001, timeElapsed / 20)
+    // Вычисляем точное время полёта до точки краша
     let flightDuration = 0;
     if (targetMultiplier > 1.00) {
         flightDuration = (20 * Math.log(targetMultiplier)) / Math.log(1.001);
@@ -90,30 +93,78 @@ io.on('connection', (socket) => {
         targetMultiplier: targetMultiplier
     });
 
+    // Регистрация пользователя по username (чтобы админ мог его найти системно)
+    socket.on('register', (username) => {
+        if (username) {
+            connectedUsers[username.toLowerCase()] = socket.id;
+        }
+    });
+
     // Обработка и трансляция ставки игрока остальным участникам
     socket.on('bet', (data) => {
         socket.broadcast.emit('bet', data);
     });
 
-    // Обработка и трансляция вывода (кэшаута) игрока остальным участникам
+    // Обработка и трансляция вывода (кэшаута) игрока
     socket.on('cashOut', (data) => {
         socket.broadcast.emit('cashOut', data);
     });
+
+    // ===========================================
+    // СИСТЕМНАЯ АДМИН ПАНЕЛЬ
+    // ===========================================
+    
+    // Запрос от админа на получение текущей статистики игрока
+    socket.on('request_user_stats', (targetUser) => {
+        const targetSocketId = connectedUsers[targetUser.toLowerCase()];
+        if (targetSocketId) {
+            // Запрашиваем статистику у самого игрока
+            io.to(targetSocketId).emit('get_stats_for_admin', socket.id);
+        } else {
+            socket.emit('admin_error', { msg: `Игрок ${targetUser} не в сети! Чтобы выдать предмет системно, он должен быть онлайн.` });
+        }
+    });
+
+    // Пересылка статистики от игрока обратно админу
+    socket.on('send_stats_to_admin', (data) => {
+        io.to(data.adminSocketId).emit('admin_user_stats', data);
+    });
+
+    // Отправка команды на пополнение баланса или выдачу NFT
+    socket.on('admin_action', (data) => {
+        const targetUser = data.targetUser.toLowerCase();
+        const targetSocketId = connectedUsers[targetUser];
+
+        if (targetSocketId) {
+            // Отправляем команду системно напрямую целевому игроку
+            io.to(targetSocketId).emit('admin_receive', data);
+            socket.emit('admin_success', { msg: `Действие успешно применено к игроку ${data.targetUser}!` });
+        } else {
+            socket.emit('admin_error', { msg: `Игрок ${data.targetUser} покинул игру!` });
+        }
+    });
+
+    // ===========================================
 
     socket.on('disconnect', () => {
         onlineCount--;
         if (onlineCount < 0) onlineCount = 0;
         io.emit('updateOnline', onlineCount);
+
+        // Удаляем пользователя из системы при выходе
+        for (let uname in connectedUsers) {
+            if (connectedUsers[uname] === socket.id) {
+                delete connectedUsers[uname];
+                break;
+            }
+        }
     });
 });
 
 const PORT = process.env.PORT || 3000;
 const path = require('path');
 
-// Укажите, что статические файлы лежат в папке, где находится server.js
 app.use(express.static(path.join(__dirname)));
-
-// Маршрут для отдачи index.html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
